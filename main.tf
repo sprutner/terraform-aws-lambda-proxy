b657ffb6be5b17958b193281361ca27cd6d82cdb
srutner <srutner@credomobile.com>
2017-05-19 15:55:52 -0700

    added name var to role

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_lambda_function" "lambda" {
 #Inline policy allows lambda to assume this role.
 
 resource "aws_iam_role" "lambda_role" {
-  name = "lambda_role"
+  name = "lambda_role_${var.name}"
 
   assume_role_policy = <<POLICY
 {

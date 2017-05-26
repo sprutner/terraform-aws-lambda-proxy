a29af910c60f3c1d09080bcf4d23941913195513
srutner <srutner@credomobile.com>
2017-05-25 19:32:21 -0600

    changed to use aws_proxy

--- main.tf
+++ main.tf
@@ -11,12 +11,12 @@ data "aws_caller_identity" "current" {}
 
 #Create up our Lambda function to proxy requests to our VPC
 resource "aws_lambda_function" "lambda" {
-  filename         = "passthrough_api.zip"
-  function_name    = "passthrough_api_${var.name}"
+  filename         = "proxy_api.zip"
+  function_name    = "proxy_api_${var.name}"
   role             = "${aws_iam_role.lambda_role.arn}"
   handler          = "index.myHandler"
   runtime          = "nodejs6.10"
-  source_code_hash = "${base64sha256(file("passthrough_api.zip"))}"
+  source_code_hash = "${base64sha256(file("proxy_api.zip"))}"
   vpc_config       = {
     subnet_ids = ["${var.subnet_ids}"]
     security_group_ids = ["${var.security_group_ids}"]
@@ -115,120 +115,43 @@ resource "aws_iam_policy_attachment" "lambda_vpc" {
 
 #Initialize the REST API
 resource "aws_api_gateway_rest_api" "api_gw" {
-  name          = "microservices_api"
+  name          = "${var.name}_microservices_api"
   description   = "API Gateway to talk to microservices"
 }
 
-#Set up root v1 resource path
-resource "aws_api_gateway_resource" "v1" {
+#Set up proxy resource path
+resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = "${aws_api_gateway_rest_api.api_gw.id}"
   parent_id   = "${aws_api_gateway_rest_api.api_gw.root_resource_id}"
-  path_part   = "v1"
+  path_part   = "{proxy+}"
 }
-
-#Set up services resource path
-resource "aws_api_gateway_resource" "v1_services" {
-  rest_api_id = "${aws_api_gateway_rest_api.api_gw.id}"
-  parent_id   = "${aws_api_gateway_resource.v1.id}"
-  path_part   = "services"
-}
-
 
 ##########POST TO A MICROSERVICE FLOW##############
 
-#Set up {service-name} POST resource path
-resource "aws_api_gateway_resource" "v1_services_service-name" {
+#Method to for ANY on the proxy resource
+resource "aws_api_gateway_method" "proxy" {
   rest_api_id = "${aws_api_gateway_rest_api.api_gw.id}"
-  parent_id   = "${aws_api_gateway_resource.v1_services.id}"
-  path_part   = "{service_name}"
-}
-
-#Method to POST to the {service_name}
-resource "aws_api_gateway_method" "v1_services_service-name_POST" {
-  rest_api_id = "${aws_api_gateway_rest_api.api_gw.id}"
-  resource_id = "${aws_api_gateway_resource.v1_services_service-name.id}"
-  http_method = "POST"
+  resource_id = "${aws_api_gateway_resource.proxy.id}"
+  http_method = "ANY"
   authorization = "NONE"
 }
 
-#Integration to invoke lambda to pass-through data to fabio (proxy)
+#Integration to invoke lambda proxy
 resource "aws_api_gateway_integration" "v1_services_service-name_POST_integration" {
   rest_api_id             = "${aws_api_gateway_rest_api.api_gw.id}"
   resource_id             = "${aws_api_gateway_resource.v1_services_service-name.id}"
   http_method             = "${aws_api_gateway_method.v1_services_service-name_POST.http_method}"
   integration_http_method = "POST"
-  type                    = "AWS"
+  type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.lambda.arn}/invocations"
-
-  request_templates {
-    "application/json" = <<EOF
-#set($allParams = $input.params())
-{
-  "requestParams" : {
-    "hostname" : "${var.fabio_hostname}",
-    "port" : "${var.fabio_port}",
-    "path" : "/$input.params('service-name')",
-    "method" : "$context.httpMethod"
-  },
-  "bodyJson" : $input.json('$'),
-  "params" : {
-    #foreach($type in $allParams.keySet())
-      #set($params = $allParams.get($type))
-      "$type" : {
-        #foreach($paramName in $params.keySet())
-          "$paramName" : "$util.escapeJavaScript($params.get($paramName))"
-          #if($foreach.hasNext),#end
-        #end
-      }
-      #if($foreach.hasNext),#end
-    #end
-  },
-  "stage-variables" : {
-    #foreach($key in $stageVariables.keySet())
-      "$key" : "$util.escapeJavaScript($stageVariables.get($key))"
-      #if($foreach.hasNext),#end
-    #end
-  },
-  "context" : {
-    "account-id" : "$context.identity.accountId",
-    "api-id" : "$context.apiId",
-    "api-key" : "$context.identity.apiKey",
-    "authorizer-principal-id" : "$context.authorizer.principalId",
-    "caller" : "$context.identity.caller",
-    "cognito-authentication-provider" : "$context.identity.cognitoAuthenticationProvider",
-    "cognito-authentication-type" : "$context.identity.cognitoAuthenticationType",
-    "cognito-identity-id" : "$context.identity.cognitoIdentityId",
-    "cognito-identity-pool-id" : "$context.identity.cognitoIdentityPoolId",
-    "http-method" : "$context.httpMethod",
-    "stage" : "$context.stage",
-    "source-ip" : "$context.identity.sourceIp",
-    "user" : "$context.identity.user",
-    "user-agent" : "$context.identity.userAgent",
-    "user-arn" : "$context.identity.userArn",
-    "request-id" : "$context.requestId",
-    "resource-id" : "$context.resourceId",
-    "resource-path" : "$context.resourcePath"
-  }
-}
-EOF
-  }
 }
 
-#The method response after receiving the 200 from Lambda
+#The method response after receiving the 200 passed up through lambda from the service
 resource "aws_api_gateway_method_response" "v1_services_service-name_POST_method_response" {
   rest_api_id = "${aws_api_gateway_rest_api.api_gw.id}"
-  resource_id = "${aws_api_gateway_resource.v1_services_service-name.id}"
-  http_method = "${aws_api_gateway_method.v1_services_service-name_POST.http_method}"
+  resource_id = "${aws_api_gateway_resource.proxy.id}"
+  http_method = "${aws_api_gateway_method.proxy.http_method}"
   status_code = "200"
-}
-
-#The integration response from fabio
-resource "aws_api_gateway_integration_response" "v1_services_service-name_POST_integration_response" {
-  depends_on  = ["aws_api_gateway_integration.v1_services_service-name_POST_integration"]
-  rest_api_id = "${aws_api_gateway_rest_api.api_gw.id}"
-  resource_id = "${aws_api_gateway_resource.v1_services_service-name.id}"
-  http_method = "${aws_api_gateway_method.v1_services_service-name_POST.http_method}"
-  status_code = "${aws_api_gateway_method_response.v1_services_service-name_POST_method_response.status_code}"
 }
 
 #Give lambda a 'trigger' permission to allow this API endpoint to invoke it
@@ -245,7 +168,7 @@ resource "aws_lambda_permission" "apigw_lambda_post_job_docker" {
 ##Deploy
 
 #Deployment for API
-resource "aws_api_gateway_deployment" "deployment" {
+resource "aws_api_gateway_deployment" "v1" {
   rest_api_id = "${aws_api_gateway_rest_api.api_gw.id}"
-  stage_name  = "test"
+  stage_name  = "v1"
 }

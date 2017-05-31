4ba427d95ac481d2b548fb4e79aa44cc3f4652db
srutner <srutner@credomobile.com>
2017-05-30 17:03:17 -0700

    added api key requirement on method

--- main.tf
+++ main.tf
@@ -136,10 +136,11 @@ resource "aws_api_gateway_resource" "proxy" {
 
 #Method to for ANY on the proxy resource
 resource "aws_api_gateway_method" "proxy" {
-  rest_api_id = "${aws_api_gateway_rest_api.api_gw.id}"
-  resource_id = "${aws_api_gateway_resource.proxy.id}"
-  http_method = "ANY"
-  authorization = "NONE"
+  rest_api_id       = "${aws_api_gateway_rest_api.api_gw.id}"
+  resource_id       = "${aws_api_gateway_resource.proxy.id}"
+  http_method       = "ANY"
+  authorization     = "NONE"
+  api_key_required  = true
 }
 
 #Integration to invoke lambda proxy

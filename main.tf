a70e2a09d23bd6379f4b2cb2a66e9a17f2ae974d
srutner <srutner@credomobile.com>
2017-08-13 22:55:21 -0700

    added depends_on for integration

--- main.tf
+++ main.tf
@@ -144,7 +144,7 @@ resource "aws_lambda_permission" "apigw_lambda_proxy" {
 
 # Deployment for API
 resource "aws_api_gateway_deployment" "v1" {
-  depends_on  = ["aws_api_gateway_method.proxy"]
+  depends_on  = ["aws_api_gateway_method.proxy", "aws_api_gateway_integration.proxy"]
   rest_api_id = "${aws_api_gateway_rest_api.api_gw.id}"
   stage_name  = "v1"
 }
